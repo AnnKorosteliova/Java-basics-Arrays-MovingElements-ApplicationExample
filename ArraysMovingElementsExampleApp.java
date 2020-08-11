8e308a26f8a85cf017db9e10be34921e60b67308
AnnKorosteliova <66014607+AnnKorosteliova@users.noreply.github.com>
2020-08-11 07:46:39 +0300

    Add files via upload

---
+++ ArraysMovingElementsExampleApp.java
@@ -0,0 +1,39 @@
+package main;
+
+public class ArraysMovingElementsExampleApp {
+
+	public static void main(String[] args) {
+
+		Integer[] data = {100, 200, 300, 400, null};
+		data = gradualElementRemoving(data);
+		data = gradualElementRemoving(data);
+		data = gradualElementRemoving(data);
+		
+		for (Integer integer : data) {
+			System.out.println(integer);
+		}
+	}
+	
+	
+	/**
+	 * @param array of Integer[] type
+	 * @return modified array if Integer[] type
+	 * The last value of an array have been removed
+	 * All elements of this array had moved at the next position 
+	 * The first value of this array become "null"  
+	 */
+	public static Integer[] gradualElementRemoving(Integer[] array) {
+		int i = array.length-1;
+		//while(i > 0) {
+		//	array[i--] = array[i];
+		//}
+		for(int d = 0; i > 0; d++) {
+			array[i--] = array[i];
+		}
+		array[i] = null;
+			return array;
+	}
+	
+	
+
+}
